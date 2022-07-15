aab5db51be341f356428fb9ad0dfdd5a6d31ed99
Nadine101 <nadinet026@gmail.com>
2022-07-14 19:03:24 -0600

    app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+let header = document.querySelector('#header');
+header[innerText] = 'miss understand';
+let main = document.querySelectorAll ('.main');
+
+let footer
+
+
+let head_content = document.getElementById('head_contnt');
+head_content[innerText] = 'yes this is right'
+let main_page = document.getElementsByClass (main_page)
+counter = 0;
